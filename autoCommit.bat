5abd61a412d8e676da6c1beec5c95b2e2021e39e
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-01-29 17:06:16 +0100

    autoCommit 2024/01/29. Hora: 17:06

--- autoCommit.bat
+++ autoCommit.bat
@@ -1,6 +1,7 @@
 cd S:\Clase
 git add --all
-git commit -m "autoCommit %date:~-4%/%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "autoCommit %date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2%"
 git push
+cls
 echo Commit realizado manin
 exit
