0fdbfcce55e73e290471d701c3c768dad2a47a4c
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-03-18 11:42:47 +0100

    Commit 2024/03/18 Hora: 11:41

---
+++ Programación/Tema7/src/Alumno/Alumno.java
@@ -0,0 +1,96 @@
+package Alumno;
+
+import java.util.Objects;
+
+public class Alumno {
+   private String nombre;
+   private String apellidos;
+   private int edad;
+   private String email;
+   private String telefono;
+
+    public Alumno(){
+
+    }
+
+    public Alumno(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public Alumno(String nombre, String apellidos, int edad, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", edad=" + edad +
+                ", email='" + email + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return edad == alumno.edad && Objects.equals(nombre, alumno.nombre) && Objects.equals(apellidos, alumno.apellidos)
+                && Objects.equals(email, alumno.email) && Objects.equals(telefono, alumno.telefono);
+    }
+
+    public void mayorEdad (Alumno alumno) {
+        if (alumno.getEdad() >= 18) {
+            System.out.println("Es mayor de edad");
+        }
+        else {
+            System.out.println("No es mayor de edad");
+        }
+    }
+
+
+}
---
+++ Programación/Tema7/src/Alumno/Main.java
@@ -0,0 +1,15 @@
+package Alumno;
+
+public class Main {
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno("Federico", "García", 17, "fede@gmail.com",
+                "982828282");
+        Alumno alumno2 = new Alumno("Pepe", "Gutiérrez", 23);
+
+       System.out.println(alumno1);
+       System.out.println(alumno2);
+
+
+
+    }
+}
