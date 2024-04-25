80c05a4b87fee0599c9936e890c37cc582a9beea
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-04-25 21:39:51 +0200

    Commit 2024/04/25. Hora: 21:39

---
+++ LingMarc/Tema 7/Sudoku/sudoku.js
@@ -0,0 +1,22 @@
+let celda;
+
+document.getElementById("button").onclick = function comprobar() {
+    for (let i = 1; i <= 9; i++) {
+        for(let j = 1; j <= 9; j++) {
+            celda = document.getElementById(i + "." + j);
+            if(isNaN(celda.value)) {
+                celda.style.backgroundColor = "#f0bbbb";
+            }
+            else if (celda.value > 0) {
+                celda.style.backgroundColor = "";
+            }
+            else {
+                celda.style.backgroundColor = "bdf0bb";
+            }
+        }
+
+        
+       
+    }   
+}
+
--- LingMarc/Tema 7/sudoku/Ejercicio2.js
+++ LingMarc/Tema 7/sudoku/Ejercicio2.js
@@ -1,11 +1,12 @@
 let nif = prompt("Escriba el número NIF");
-let acumulador = 0;
 let letras = "TRWAGMYFPDXBNJZSQVHLCKE"
 let resto = 0;
 
-while ((nif < 0 || isNaN(nif)) && nif.length 8) {
+while (nif < 0 || isNaN(nif) || nif.length != 8) {
+    nif = prompt("Número inválido, vueva a intentarlo.");
+}
     resto = nif % 23;
     alert("Número válido! La letra del DNI es: " + letras.charAt(resto));
-}
+
 
 
