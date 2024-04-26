1ec5f88819a4fd324a95cd6eaf5aba94e0de31ee
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-04-26 11:09:12 +0200

    Commit 2024/04/26. Hora: 11:09

--- LingMarc/Tema 7/sudoku/Ejercicio2.js
+++ LingMarc/Tema 7/sudoku/Ejercicio2.js
@@ -1,12 +1,19 @@
-let nif = prompt("Escriba el número NIF");
 let letras = "TRWAGMYFPDXBNJZSQVHLCKE"
 let resto = 0;
+let nif = prompt("Escriba el número NIF");
 
-while (nif < 0 || isNaN(nif) || nif.length != 8) {
-    nif = prompt("Número inválido, vueva a intentarlo.");
+while (nif != null) {
+    if (isNaN(nif) || nif.length != 8) {
+        alert("Número inválido, vueva a intentarlo.");
+        nif = prompt("Escriba el número NIF");
+    }
+    else {
+        resto = nif % 23;
+        alert("Número válido! La letra del DNI es: " + letras.charAt(resto));
+        nif = prompt("Escriba el número NIF");
+    }
 }
-    resto = nif % 23;
-    alert("Número válido! La letra del DNI es: " + letras.charAt(resto));
+
 
 
 
