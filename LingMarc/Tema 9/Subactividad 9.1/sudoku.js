bf990c9b5c94a6159792aa29b2231cd43d8bbbf1
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-04-30 11:22:25 +0200

    Commit 2024/04/30. Hora: 11:22

--- LingMarc/Tema 9/Subactividad 9.1/sudoku.js
+++ LingMarc/Tema 9/Subactividad 9.1/sudoku.js
@@ -1,30 +1,26 @@
-let listaCeldas = new Array();
+let celda;
 
 for (let i = 1; i <= 9; i++) {
     for (let j = 1; j <= 9; j++) {
-        let celda = document.getElementById(i + "." + j);
-        listaCeldas.push(celda);
+        let celda = document.getElementById(i + "." + j).onblur = function() {
+            comprobar(this);
+        }
+    }
+}
+
+function comprobar(celda) {
+    if (isNaN(celda.value)) {
+        celda.style.backgroundColor = "#f0bbbb";
+    }
+    else if (celda.value > 0) {
+        celda.style.backgroundColor = "";
+    }
+    else {
+        celda.style.backgroundColor = "#bdf0bb";
     }
 }
 
-for (let i = 0; i < listaCeldas.length; i++) {
-    listaCeldas[i].onblur = comprobacion;
-}
 
-function comprobacion() {
-    for (let i = 0; i < listaCeldas.length; i++) {
-        if (isNaN(listaCeldas[i].value)) {
-            listaCeldas[i].style.backgroundColor = "#f0bbbb";
-        }
-        else if (listaCeldas[i].value > 0) {
-            listaCeldas[i].style.backgroundColor = "";
-        }
-        else {
-            listaCeldas[i].style.backgroundColor = "#bdf0bb";
-        }
-    }
-
-}
 
 
 
