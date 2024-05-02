ff3da158788c5f8119ac8d3a885d56475f030e47
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-05-02 17:48:37 +0200

    Commit 2024/05/02. Hora: 17:48

--- LingMarc/Tema 9/Nueva carpeta/sudoku.js
+++ LingMarc/Tema 9/Nueva carpeta/sudoku.js
@@ -1,6 +1,8 @@
 let celda;
 let listaCeldas = new Array(9);
 let comprobacion = document.getElementById("comprobacion");
+let respuesta = "";
+
 
 document.getElementById("button").onclick = function comprobar() {
 
@@ -11,11 +13,11 @@ document.getElementById("button").onclick = function comprobar() {
             celda = document.getElementById(i + "." + j);
             for (let k = 0; k < 9; k++) {
                 if (celda.value === "") {
-                celda.style.backgroundColor = "#bdf0bb";
+                    celda.style.backgroundColor = "#bdf0bb";
                 } else if (listaCeldas[k] == celda.value) {
                     celda.style.backgroundColor = "#f0bbbb";
                     comprobacion.innerHTML = celda.id + " se repite";
-                    
+
                 } else if (isNaN(celda.value)) {
                     celda.style.backgroundColor = "#f0bbbb";
                 }
@@ -28,7 +30,7 @@ document.getElementById("button").onclick = function comprobar() {
     }
 
     // Comprobación columnas //
-    
+
     for (let i = 1; i <= 9; i++) {
         listaCeldas.fill(0);
         for (let j = 1; j <= 9; j++) {
@@ -36,7 +38,7 @@ document.getElementById("button").onclick = function comprobar() {
             for (let k = 0; k < 9; k++) {
                 if (celda.value === "") {
                     celda.style.backgroundColor = "#bdf0bb";
-            } else if (listaCeldas[k] == celda.value) {
+                } else if (listaCeldas[k] == celda.value) {
                     celda.style.backgroundColor = "#f0bbbb";
                     comprobacion.innerHTML = celda.id + " se repite";
                 } else {
@@ -46,13 +48,29 @@ document.getElementById("button").onclick = function comprobar() {
         }
     }
 
-    // Comprobación grupos // 
+    // Comprobación grupos
 
+    let bloque = true;
     for (let i = 1; i <= 9; i++) {
-        let bloque = document.getElementsByClassName("bloque" + i);
+        let numeros = [];
+        let celdas = document.getElementsByClassName('bloque' + i);
+        for (let j = 0; j < celdas.length; j++) {
+            let celda = celdas[j];
+            if (celda.value == "") {
+                let nuevop = document.createElement('p');
+                nuevop.innerHTML = 'Una casilla del bloque ' + i + " contiene un espacio en blanco.";
+                celda.style.backgroundColor = '#8DF961';
+                document.querySelector('footer').appendChild(nuevop);
+                bloque = false;
 
-        listaCeldas.fill(0);
-
-        
+            } else if (isNaN(celda.value) || celda.value == 0) {
+                let nuevop = document.createElement('p');
+                nuevop.innerHTML = 'Error en el bloque ' + i + '.';
+                celda.style.backgroundColor = '#F57757';
+                document.querySelector('footer').appendChild(nuevop);
+                bloque = false;
+            }
+        }
     }
 }
+
