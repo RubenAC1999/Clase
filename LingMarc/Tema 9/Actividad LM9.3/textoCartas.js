14083f7750a0efcbdb280b1f673ce33dae49d7fc
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-05-08 11:24:16 +0200

    Commit 2024/05/08. Hora: 11:24

--- LingMarc/Tema 9/Actividad LM9.3/textoCartas.js
+++ LingMarc/Tema 9/Actividad LM9.3/textoCartas.js
@@ -2,9 +2,9 @@ let palos = ["oros", "espadas", "copas", "bastos"];
 
 for (let i = 0; i < palos.length; i++) {
     for (let j = 1; j <= 12; j++) {
-        let carta = document.createElement("img");
-        carta.setAttribute("src", "cartas/" + palos[i].charAt(0) + j + ".jpg");
+        let carta = document.createElement("p");
         carta.setAttribute("palo", palos[i]);
+        carta.appendChild(document.createTextNode(j + " de " + palos[i]));
         document.body.appendChild(carta); 
     }
 }
