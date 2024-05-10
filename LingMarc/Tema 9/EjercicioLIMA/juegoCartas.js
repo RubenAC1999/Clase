df9321c4ba8af94f7bcfd6d69fb3f7c1ce43484f
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-05-10 10:40:02 +0200

    Commit 2024/05/10. Hora: 10:40

--- LingMarc/Tema 9/EjercicioLIMA/juegoCartas.js
+++ LingMarc/Tema 9/EjercicioLIMA/juegoCartas.js
@@ -1,7 +1,31 @@
-let palos = ["o", "e", "c", "b"];
-let imagenes = new Array(16);
-let imagenesRandom = new Array(16);
-let numeros = new Array(16);
+let imagenes = [];
+let imagenesRandom = [];
+let numeros = []
+
+
+for (let i = 1; i < 7; i++) {
+ let carta = document.createElement("img");
+ carta.setAttribute("src", "cartas/carta" + i + ".jpg");
+ imagenes.push(carta);
+
+ let cartaCopia = document.createElement("img");
+ cartaCopia.setAttribute("src", "cartas/carta" + i + ".jpg");
+ imagenes.push(cartaCopia);
+}
+
+for (let i = 0; i < imagenes.length; i++) {
+    let numAleatorio = getRandomInt(0,imagenes.length);
+    if (!numeros.includes(numAleatorio)) {
+            numeros.push(numAleatorio);
+            imagenesRandom.push(imagenes[numAleatorio]);
+    }
+    else {
+        i--;
+    }
+}
+
+imagenesRandom.forEach(mostrarCartas);
+
 
 function getRandomInt(min, max) { 
     return Math.floor(Math.random() * (max - min)) + min; 
@@ -10,33 +34,6 @@ function getRandomInt(min, max) {
 function mostrarCartas(cartaActual) {
     document.body.appendChild(cartaActual);
 }
-
-
-
-for (let i = 1; i <= 8; i++) {
- let paloAleatorio = palos[getRandomInt(0, (palos.length - 1))]
- let numCarta = getRandomInt(1, 13);
- let carta = document.createElement("img");
- let cartaCopia = document.createElement("img");
- carta.setAttribute("src", "cartas/" + paloAleatorio + numCarta + ".jpg");
- cartaCopia.setAttribute("src", "cartas/" + paloAleatorio + numCarta + ".jpg");
- imagenes.push(carta);
- imagenes.push(cartaCopia);
-}
-
-for (let i = 0; i < 16; i++) {
-    let numAleatorio = getRandomInt(0,16);
-    if (numeros.includes(numAleatorio) == false) {
-            numeros.push(numAleatorio);
-            imagenesRandom.push(imagenes[numAleatorio]);
-    }
-}
-
-imagenes.forEach(mostrarCartas);
-imagenesRandom.forEach(mostrarCartas);
-
-
-
 
 
 
