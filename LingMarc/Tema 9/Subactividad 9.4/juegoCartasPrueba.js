7502a373b1c0ca07af53cc36fae8ec5e821b111e
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-05-14 11:42:39 +0200

    Commit 2024/05/14. Hora: 11:42

--- LingMarc/Tema 9/Subactividad 9.4/juegoCartasPrueba.js
+++ LingMarc/Tema 9/Subactividad 9.4/juegoCartasPrueba.js
@@ -2,6 +2,12 @@ class Carta {
     constructor(src, valor) {
         this.src = src;
         this.valor = valor;
+        this.crearElemento = function() {
+            this.elemento = document.createElement("img");
+            this.elemento.setAttribute("src", this.src);
+            this.elemento.setAttribute("valor", this.valor);
+            return this.elemento;
+        }
     }
 }
 
@@ -9,39 +15,22 @@ function crearBaraja() {
     let baraja = [];
     for (let i = 1; i < 7; i++) {
         let carta = new Carta("cartas/dorso.jpg", "cartas/carta" + i + ".jpg");
-
-        carta = document.createElement("img");
-        carta.setAttribute("src", carta.src);
-        carta.setAttribute("alt", carta.valor);
-        baraja.push(carta);
-
+        baraja.push(carta.crearElemento());
         let cartaCopia = new Carta("cartas/dorso.jpg", "cartas/carta" + i + ".jpg");
-        cartaCopia = document.createElement("img");
-        cartaCopia.setAttribute("src", cartaCopia.src);
-        cartaCopia.setAttribute("alt", cartaCopia.valor);
-        baraja.push(cartaCopia);
+        baraja.push(cartaCopia.crearElemento());
     }
     return baraja;
 }
 
-crearBaraja().forEach(mostrar);
-let numeros = [];
+let baraja = crearBaraja();
+baraja.forEach(agregarEvento);
 
-function barajar(baraja) {
-    let cartasBarajadas = [];
-    for (let i = 0; i < baraja.length; i++) {
-        let numAleatorio = getRandomInt(0, baraja.length);
-        if (!numeros.includes(numAleatorio)) {
-            numeros.push(numAleatorio);
-            cartasBarajadas.push(baraja[numAleatorio]);
-        }
-        else {
-            i--;
-        }
-    }
-    return cartasBarajadas;
-}
+let primerasCartas = baraja.slice(0, 6);
+let ultimasCartas = baraja.slice(6);
 
+primerasCartas.forEach(mostrar);
+document.body.appendChild(document.createElement("br"));
+ultimasCartas.forEach(mostrar);
 
 function mostrar(carta) {
     document.body.appendChild(carta);
@@ -51,5 +40,50 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
+
+let girando = false;
+let cartasGiradas = [];
+
+function darVuelta() {
+    if (girando) {
+        return;
+    }
+    
+    let dorso = this.getAttribute("src");
+    this.setAttribute("src", this.getAttribute("valor"));
+    cartasGiradas.push(this);
+
+    if (cartasGiradas.length === 2) {
+        girando = true;
+        primerasCartas.forEach(bloquear);
+        ultimasCartas.forEach(bloquear);
+
+        setTimeout(function () {
+            if (cartasGiradas[0].getAttribute("src") === cartasGiradas[1].getAttribute("src")) {
+                cartasGiradas[0].classList.add("carta-emparejada");
+                cartasGiradas[1].classList.add("carta-emparejada");
+            } else {
+                cartasGiradas.forEach(function (carta) {
+                    carta.setAttribute("src", dorso);
+                });
+            }
+
+            cartasGiradas = [];
+            primerasCartas.forEach(agregarEvento);
+            ultimasCartas.forEach(agregarEvento);
+
+            girando = false;
+        }, 1500);
+    }
+}
+
+    function bloquear() {
+        this.removeEventListener("click", darVuelta);
+    }
+
+    function agregarEvento(carta) {
+        carta.addEventListener("click", darVuelta);
+    }
 
 
