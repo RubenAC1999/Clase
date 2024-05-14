afef4bc60fb695c5f64202c247d2dde33a9d01be
Rubén Agra Casal <151959870+RubenAC1999@users.noreply.github.com>
2024-05-14 21:55:01 +0200

    autoCommit 2024/05/14. Hora: 21:55

--- ContorDesenv/ExerciciosRefactor/Tema8/src/tarea3/GestorDB.java
+++ ContorDesenv/ExerciciosRefactor/Tema8/src/tarea3/GestorDB.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 import java.util.Scanner;
 
 public class GestorDB {
-    public Connection connection = null;
+    private Connection connection;
     public GestorDB() {
         Connection conn = null;
         try {
@@ -16,29 +16,36 @@ public class GestorDB {
         }
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_tareas_db",
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_tareas_db",
                     "usuarioGlobal", "abc123.");
-            boolean isValid = connection.isValid(5000);
+            boolean isValid = conn.isValid(5000);
             System.out.println(isValid? "Conexión exitosa" : "Error en la conexión");
 
         } catch (SQLException e) {
-            connection = null;
+
             System.out.println(e);
         }
         connection = conn;
     }
 
 
-    public static void iniciar() {
+    public static String iniciar() {
         String[] opciones = {"Agregar Tarea", "Marcar tarea como completada", "Ver lista de tareas", "Eliminar tarea"};
         String opcion = (String) JOptionPane.showInputDialog(null, "Seleccione una opción",
                 "Gestión de tareas", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+        return opcion;
     }
 
-    public static void agregarTarea(GestorDB basedatos) {
+    public static void agregarTarea(GestorDB basedatos) throws SQLException {
         Scanner scanner = new Scanner(System.in);
+        Statement st = basedatos.connection.createStatement();
         System.out.println("Introduzca el nombre de la tarea");
         String nombreTarea = scanner.nextLine();
-        Statement st = basedatos.connection.createStatement("")
+        System.out.println("Introduzca la descripción de la tarea");
+        String descripcion = scanner.nextLine();
+        st.executeUpdate("INSERT INTO Tareas (nombre, descripcion) VALUES ('" + nombreTarea + "', '" + descripcion + "');");
+        st.close();
+        System.out.println("La tarea se ha registrado correctamente.");
     }
 }
--- ContorDesenv/ExerciciosRefactor/Tema8/src/tarea3/Main.java
+++ ContorDesenv/ExerciciosRefactor/Tema8/src/tarea3/Main.java
@@ -1,10 +1,22 @@
 package tarea3;
 
+import java.sql.SQLException;
+
 import static tarea3.GestorDB.*;
 
 public class Main {
     public static void main(String[] args) {
+        GestorDB basedatos = new GestorDB();
 
-        iniciar();
+        switch (iniciar()) {
+            case "Agregar Tarea":
+                try {
+                    agregarTarea(basedatos);
+                } catch (SQLException e) {
+                    System.out.println(e);
+                }
+
+        }
+
     }
 }
