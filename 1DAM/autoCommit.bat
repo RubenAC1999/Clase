521f659913ea6e0fdb2fdbd57ce5216b84707bfb
Rubén Agra Casal <rubenagra99@gmail.com>
2024-09-12 21:46:48 +0200

    autoCommit 2024/09/12. Hora: 21:46

--- 1DAM/autoCommit.bat
+++ 1DAM/autoCommit.bat
@@ -1,4 +1,4 @@
-cd C:\Users\dam1_alu01\Desktop\Clase
+cd C:\Users\DAM2_Alu01\Documents\GitHub\Clase\2DAM
 git add --all
 git commit -m "autoCommit %date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2%"
 git push
