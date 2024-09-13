e17b4560c49a5167312c0fc794b109ced86f2252
Rubén Agra Casal <rubenagra99@gmail.com>
2024-09-13 21:53:13 +0200

    autoCommit 2024/09/13. Hora: 21:53

--- 2DAM/autoCommit.bat
+++ 2DAM/autoCommit.bat
@@ -2,5 +2,4 @@ cd C:\Users\DAM2_Alu01\Documents\GitHub\Clase\2DAM
 git add --all
 git commit -m "autoCommit %date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2%"
 git push
-texto.txt
-exit
+echo "Commit realizado correctamente"
