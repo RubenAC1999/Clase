081cadf1d1e74fce65795de7c4d2d244a01a223f
Rubén <151959870+RubenAC1999@users.noreply.github.com>
2025-01-09 21:50:49 +0100

    autoCommit 2025/01/09. Hora: 21:50

--- 2DAM/Acceso a datos/HeidiSQL_12.8_64_Portable/Backups/query-tab-2024-11-21_16-44-33-298.sql
+++ 2DAM/Acceso a datos/HeidiSQL_12.8_64_Portable/Backups/query-tab-2024-11-21_16-44-33-298.sql
@@ -1,1 +1,9 @@
-SELECT * FROM parkingsgarajes WHERE disponibleAlquiler = 1;
+SELECT e.dni, e.nom_emp, d.nom_depto, s.nom_sede, p.categoria, p.sueldo_bruto_anual 
+
+FROM empleado e 
+
+JOIN departamento d ON e.id_depto = d.id_depto 
+
+JOIN sede s ON d.id_sede = s.id_sede
+
+JOIN empleado_datos_prof p ON e.dni = p.dni;
